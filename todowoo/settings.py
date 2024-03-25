789a63b2a4f4ea2c7d58df1141c6906a36f3d9a0
YashnarbekS <122490631+Programmeryashnar@users.noreply.github.com>
2024-03-25 08:57:34 +0500

    newone

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_jjhm#&qjqbm_qula%xs2(0&^qwai@2c%au&4z57%g+=-lsbno'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
